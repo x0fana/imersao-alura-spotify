19fed320c3b0a131f3471b1577c3bd5245564c63
x0fana <giovana.santos@grad.ufsc.br>
2024-01-28 01:30:54 -0300

    atualizacao

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const resultArtist = document.getElementById('result-artist');
 const resultPlaylist = document.getElementById('result-playlists');
 
 function requestApi(searchTerm) {
-    const url = `https://spotify-alura.vercel.app/artists?name_like=${searchTerm}`
+    const url = `https://imersao-alura-spotify-seven.vercel.app/artists?name_like=${searchTerm}`
     fetch(url)
       .then((response) => response.json())
       .then((result) => displayResults(result))
